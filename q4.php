287902233cdbfb7b73de27e8fdea0991d0ccffda
Pasindu Jay <124522613+pasinduNj@users.noreply.github.com>
2024-05-06 03:44:02 +0530

    Add files via upload

---
+++ q4.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+$fruit=["apple","banana","orange","grape","pineapple"];
+
+for($i=0;$i<5;$i++){
+    echo "$fruit[$i] is a fruit <br>";
+}
+    
